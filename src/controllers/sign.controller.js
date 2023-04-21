50a3a63b05b20cc78c4d1395b464acfb5dd12305
yurilopesm <yurilopesm@hotmail.com>
2023-04-21 18:43:50 -0300

    SignIn route created

--- src/controllers/sign.controller.js
+++ src/controllers/sign.controller.js
@@ -1,6 +1,7 @@
 import bcrypt from 'bcrypt'
 import { db } from '../app.js'
-import { signupSchema } from '../schemas/sign.schema.js'
+import { signinSchema, signupSchema } from '../schemas/sign.schema.js'
+import { v4 as uuid } from 'uuid'
 
 export async function signup(req, res) {
     const {name, email, password} = req.body
@@ -26,5 +27,24 @@ export async function signup(req, res) {
     }
 }
 export async function signin(req, res) {
-    
+    const {email, password} = req.body
+    const validBody = signinSchema.validate({
+        email,
+        password
+    })
+    if(validBody.error) return res.sendStatus(422)
+    try {
+        const user = await db.collection("users").findOne({email: email})
+        if(!user) return res.sendStatus(404)
+        const validPassword = bcrypt.compareSync(password, user.password)
+        if(!validPassword) return res.sendStatus(401)
+        const token = uuid()
+        db.collection("sessions").insertOne({
+            id: user._id,
+            token
+        })
+        return res.status(200).send(token)
+    } catch(error) {
+        res.status(500).send(error.message)
+    }
 }
